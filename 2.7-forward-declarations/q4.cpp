784442a2cc524fbab955e5ea952605b59241f9ed
udkfer <udkfer@gmail.com>
2026-02-16 17:22:10 -0300

    2.7 — Forward declarations and definitions

---
+++ 2.7-forward-declarations/q4.cpp
@@ -0,0 +1,23 @@
+// Question #4
+//
+// Write the function declaration for this function (use the preferred form with names):
+//
+// int doMath(int first, int second, int third, int fourth)
+// {
+//      return first + second * third / fourth;
+// }
+
+#include <iostream>
+
+int doMath(int first, int second, int third, int fourth);
+
+int doMath(int first, int second, int third, int fourth)
+{
+     return first + second * third / fourth;
+}
+
+int main()
+{
+  int doMath;
+  return 0;
+}
