7163bd8ae116ac1319fa7bb80c8e9d0d4e4c1f4d
udkfer <udkfer@gmail.com>
2026-03-24 19:41:47 -0300

    4.12 — Introduction to type conversion and static_cast

---
+++ Chapter 4 Fundamental Data Types/4.12 — Introduction to type conversion and static_cast/Question #2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdint>
+
+int main()
+{
+  std::cout << "Enter a single character: \n";
+  char c{};
+  std::cin >> c;
+
+  std::cout << "You entered \'" << c << "\' which has ASCII code " << (int)c << ".\n";
+
+  return 0;
+}
