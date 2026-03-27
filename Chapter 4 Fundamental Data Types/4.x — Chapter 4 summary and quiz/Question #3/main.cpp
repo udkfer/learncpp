7c9910c7dfb783c120fffadda7130fa25bae1b3a
udkfer <udkfer@gmail.com>
2026-03-27 11:56:25 -0300

    Question #3

--- Chapter 4 Fundamental Data Types/4.x — Chapter 4 summary and quiz/Question #3/main.cpp
+++ Chapter 4 Fundamental Data Types/4.x — Chapter 4 summary and quiz/Question #3/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 
+double cbh(int th, int s)
+{
+  double bh{th - (9.8 * (s * s) / 2.0)};
+  if (bh < 0.0)
+    return 0.0;
+  return bh;
+}
+
+void pbh(double bh, int s)
+{
+  if (bh > 0.0)
+    std::cout << "At " << s << " seconds, the ball is at height: " << bh << " meters\n";
+  else
+    std::cout << "At " << s << " seconds, the ball is on the ground.\n";
+}
+
+void cpbh(double th, int s)
+{
+  double bh{ cbh(th, s) };
+  pbh(bh, s);
+}
+
 int main()
 {
   std::cout << "Enter the height of the tower in meters: ";
-  float h{};
-  std::cin >> h;
+  int th{};
+  std::cin >> th;
 
-  // distance fallen = gravity_constant * x_seconds2 / 2
-  double {};
+  cpbh(th, 0);
+  cpbh(th, 1);
+  cpbh(th, 2);
+  cpbh(th, 3);
+  cpbh(th, 4);
+  cpbh(th, 5);
 
   return 0;
 }
