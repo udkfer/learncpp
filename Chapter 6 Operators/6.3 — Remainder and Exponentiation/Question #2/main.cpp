b520d7f140b8bc678f2d7039cbd3143822c03cbd
udkfer <udkfer@gmail.com>
2026-04-13 19:33:02 -0300

    Chapter 6 Operators/6.3 — Remainder and Exponentiation/Question #2

---
+++ Chapter 6 Operators/6.3 — Remainder and Exponentiation/Question #2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+  constexpr bool isEven(int x)
+  {
+    return (x % 2) == 0;
+  }
+
+int main()
+{
+  std::cout << "Enter an integer: ";
+  int x{};
+  std::cin >> x;
+
+  if (isEven(x))
+    std::cout << x << " is even\n";
+  else
+    std::cout << x << " is odd\n";
+
+  return 0;
+}
